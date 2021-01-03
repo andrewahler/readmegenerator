02974ed896778b54df6120e00a3394c0ef13b56c
Andrew Ahler <andrewahler@gmail.com>
2021-01-03 12:06:57 -0600

    removed nodemodules

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 const inquirer = require("inquirer");
-const fs = require("fs")
+const fs = require("fs");
 
-const questions ={
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-}
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    text: "enter title of the readme",
+  },
+  {
+    type: "input",
+    name: "description",
+    text: "Write a description",
+  },
+  {
+    type: "input",
+    name: "instillation",
+    text: "Instillation instructions",
+  },
+  {
+    type: "input",
+    name: "Usage",
+    text: "Usage instructions",
+  },
+  {
+    type: "input",
+    name: "Contributing",
+    text: "Contributing section",
+  },
+  {
+    type: "input",
+    name: "License",
+    text: "Enter license information",
+  },
+  {
+    type: "input",
+    name: "Tests",
+    text: "Enter Tests",
+  },
+  {
+    type: "input",
+    name: "Github",
+    text: "enter github username",
+  },
+];
+console.log(questions)
